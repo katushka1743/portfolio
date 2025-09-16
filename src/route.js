072eb560dbabb73cdb0ea6a9cb74c775c3260a68
Екатерина Смирнова <katushka1743@yandex.ru>
2025-09-16 16:46:14 +0300

    1 commit

---
+++ src/route.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import mainPage from "./components/mainPage.vue";
+import aboutPage from "./components/aboutPage.vue";
+import casePage from "./components/casePage.vue";
+import skillsPage from "./components/skillsPage.vue";
+import contactsPage from "./components/contactsPage.vue";
+
+const routes = [
+  { path: "/", component: mainPage, name: "mainPage" },
+  { path: "/aboutPage", component: aboutPage, name: "aboutPage" },
+  { path: "/casePage", component: casePage, name: "casePage" },
+  { path: "/skillsPage", component: skillsPage, name: "skillsPage" },
+  { path: "/contactsPage", component: contactsPage, name: "contactsPage" },
+];
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior() {
+    window.scrollTo(0, 0);
+  },
+});
